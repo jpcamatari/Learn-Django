0cce31362409d9dbf8c4ad5d0529a8d1debdf676
João Paulo Antonio Camatari <jpcamatari@hotmail.com>
2022-12-28 00:29:28 -0300

    render page html

--- autenticacao/views.py
+++ autenticacao/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def cadastro(request):
-    return HttpResponse('Faça seu Cadastro')
+    return render(request, 'index.html')
 
 def auth(request):
     return HttpResponse('Você Está na autenticação')
--- learn_django/urls.py
+++ learn_django/urls.py
@@ -1,9 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
-from index import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('autenticacao/', include('autenticacao.urls')),
-    path('', views.index),
 ]
