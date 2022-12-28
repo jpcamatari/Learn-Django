619090ade71592b7fcdc7b6641e8428477eb79bd
João Paulo Antonio Camatari <jpcamatari@hotmail.com>
2022-12-27 23:49:35 -0300

    learning routes

--- learn_django/urls.py
+++ learn_django/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from index import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('autenticacao/', include('autenticacao.urls'))
+    path('autenticacao/', include('autenticacao.urls')),
+    path('', views.index),
 ]
