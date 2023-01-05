d75fbf402964892e8bece595ed48ec1399512ad0
João Paulo Antonio Camatari <jpcamatari@hotmail.com>
2023-01-04 23:35:58 -0300

    Changes and tests for coding into templates

--- autenticacao/views.py
+++ autenticacao/views.py
@@ -2,11 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def cadastro(request):
-    nome = 'João Paulo'
-    idade = 28
-    profissao = 'Programador'
+    pessoa = {'nome': 'João Paulo', 'idade': 28, 'profissao': 'Programador'}
 
-    return render(request, 'index.html', {'nome': nome, 'idade':idade, 'profissão':profissao})
+    return render(request, 'index.html', {'pessoa': pessoa})
 
 def auth(request):
     return HttpResponse('Você Está na autenticação')
