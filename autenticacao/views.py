449a1adb7c2fd73d988b8f54f6f2aa65326072f2
João Paulo Antonio Camatari <jpcamatari@hotmail.com>
2023-01-10 22:52:42 -0300

    get parameters correct

--- autenticacao/views.py
+++ autenticacao/views.py
@@ -2,7 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def cadastro(request):
-    pessoa = {'nome': 'João Paulo', 'idade': 28, 'profissao': 'Programador'}
-
     return render(request, 'index.html')
 
