7501b9ab5ddc6cccd9f1a718d35a239d0a1d9fcb
João Paulo Antonio Camatari <jpcamatari@hotmail.com>
2023-01-10 22:51:32 -0300

    GET Parameters

--- autenticacao/views.py
+++ autenticacao/views.py
@@ -4,7 +4,5 @@ from django.http import HttpResponse
 def cadastro(request):
     pessoa = {'nome': 'João Paulo', 'idade': 28, 'profissao': 'Programador'}
 
-    return render(request, 'index.html', {'pessoa': pessoa})
+    return render(request, 'index.html')
 
-def auth(request):
-    return HttpResponse('Você Está na autenticação')
