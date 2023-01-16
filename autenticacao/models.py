f080e888a1dfb1513e1adc2ddfaf57e4854625bd
João Paulo Antonio Camatari <jpcamatari@hotmail.com>
2023-01-16 18:27:38 -0300

    changes db

--- autenticacao/models.py
+++ autenticacao/models.py
@@ -3,5 +3,7 @@ from django.db import models
 class Usuario(models.Model):
     nome = models.CharField(max_length=50)
     email = models.EmailField()
+    senha = models.CharField(max_length=50)
+
     def __str__(self):
         return self.nome
