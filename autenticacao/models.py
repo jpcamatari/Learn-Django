706f2f4a2a88e34d5adb8aeb7a806d2086f51730
João Paulo Antonio Camatari <jpcamatari@hotmail.com>
2023-01-25 22:54:17 -0300

    Learn Foreign keys

--- autenticacao/models.py
+++ autenticacao/models.py
@@ -1,9 +1,14 @@
 from django.db import models
+from django.db.models.fields import CharField
+
+class Cargos(models.Model):
+    nome = CharField(max_length=50)
 
 class Usuario(models.Model):
-    nome = models.CharField(max_length=50)
+    nome = models.CharField(max_length=100)
     email = models.EmailField()
-    senha = models.CharField(max_length=50)
+    senha = models.CharField(max_length=100)
+    cargo = models.ForeignKey(Cargos, on_delete=models.DO_NOTHING)
 
     def __str__(self):
         return self.nome
