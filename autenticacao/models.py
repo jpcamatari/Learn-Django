13f8418123efa8567cc72155d9a7f3e2bc4c7e15
João Paulo Antonio Camatari <jpcamatari@hotmail.com>
2023-03-15 23:09:28 -0300

    learning function .get for take date from db

--- autenticacao/models.py
+++ autenticacao/models.py
@@ -8,7 +8,7 @@ class Usuario(models.Model):
     nome = models.CharField(max_length=100)
     email = models.EmailField()
     senha = models.CharField(max_length=100)
-    cargo = models.ForeignKey(Cargos, on_delete=models.DO_NOTHING)
+    cargo = models.ManyToManyField(Cargos)
 
     def __str__(self):
         return self.nome
