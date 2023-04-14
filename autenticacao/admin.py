0e91481d9425a9356bcd780b600bd0036ce003fa
João Paulo Antonio Camatari <jpcamatari@hotmail.com>
2023-04-13 22:00:49 -0300

    learning about permissions and changes on Admin

--- autenticacao/admin.py
+++ autenticacao/admin.py
@@ -2,7 +2,10 @@ from django.contrib import admin
 from .models import Usuario, Cargos
 
 # Register your models here.
+@admin.register(Usuario)
+class UsuarioAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'email', 'senha')
+    readonly_fields = ('senha',)
 
-admin.site.register(Usuario)
 admin.site.register(Cargos)
 
--- autenticacao/models.py
+++ autenticacao/models.py
@@ -4,6 +4,9 @@ from django.db.models.fields import CharField
 class Cargos(models.Model):
     nome = CharField(max_length=50)
 
+    def __str__(self):
+        return self.nome
+
 class Usuario(models.Model):
     nome = models.CharField(max_length=100)
     email = models.EmailField()
